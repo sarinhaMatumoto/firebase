63fc9c08889e394f0e2fa76f34bfafa3565da1f1
sarah matumoto <132668625+sarinhaMatumoto@users.noreply.github.com>
2024-03-28 12:44:55 -0300

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var  firebaseConfig = {
+    apiKey: "AIzaSyBufcsMWCqYZrLTC3s7V24ZNEKsMoxGJHw",
+    authDomain: "may-firebase-f61cf.firebaseapp.com",
+    projectId: "may-firebase-f61cf",
+    storageBucket: "may-firebase-f61cf.appspot.com",
+    messagingSenderId: "608529455677",
+    appId: "1:608529455677:web:f71e66b19fe21a07176854"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
